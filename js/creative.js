5e74a4f4c5257335a2798a50500e0b8c40ae07de
Emil <emilburaczynski@gmail.com>
2019-05-06 22:19:23 +0200

    updated

--- js/creative.js
+++ js/creative.js
@@ -102,13 +102,10 @@
     videoWidth,
     videoHeight;
 
-    // console.log(windowHeight);
-
     $(element).each(function(){
       var videoAspectRatio = $(this).data('height')/$(this).data('width');
 
       $(this).width(windowWidth);
-
       // if(windowWidth < 1000){
       //     videoHeight = windowHeight;
       //     videoWidth = videoHeight / videoAspectRatio;
@@ -116,10 +113,9 @@
 
       //     $(this).width(videoWidth).height(videoHeight);
       // }
-
       $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
     });
+    
   }
 
 
